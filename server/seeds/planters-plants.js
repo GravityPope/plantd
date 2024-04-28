f263302ef502f70eacb63d005c415167ba0fbb3d
GravityPope <hijenkins@live.ca>
2024-04-28 11:58:53 -0600

    add planters seed data

--- server/seeds/planters-plants.js
+++ server/seeds/planters-plants.js
@@ -1,7 +1,6 @@
 const typesData = require('../seed-data/types');
 const plantsData = require('../seed-data/plants');
 const cultivarsData = require('../seed-data/cultivars');
-const zonesData = require('../seed-data/zones');
 const plantersData = require('../seed-data/planters');
 
 
@@ -10,12 +9,10 @@ exports.seed = async function(knex) {
   await knex('types').del();
   await knex('plants').del();
   await knex('cultivars').del();
-  await knex('zones').del();
   await knex('planters').del();
 
   await knex('types').insert(typesData);
   await knex('plants').insert(plantsData);
   await knex('cultivars').insert(cultivarsData);
-  await knex('zones').insert(zonesData);
   await knex('planters').insert(plantersData);
 };
