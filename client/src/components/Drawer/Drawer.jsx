a713f67322231253f2f5e4c8cd467c164df9e624
GravityPope <hijenkins@live.ca>
2024-04-29 13:46:03 -0600

    add Drawer component and skeleton

---
+++ client/src/components/Drawer/Drawer.jsx
@@ -0,0 +1,113 @@
+import "./Drawer.scss";
+
+export default function Drawer(props) {
+  const { id, list, filteredList, setFilteredList } = props;
+
+  // Plant drawer
+  if (!props.dndPlantList) {
+    // no filters applied
+    if (!filteredList) {
+      return (
+        <section className="drawer">
+          <SearchFilters
+            list={list}
+            filteredList={filteredList}
+            setFilteredList={setFilteredList}
+          />
+          <ul className="plant-list">
+            {list.map((plant) => {
+              <Draggable
+                key={plant.plant_id}
+                type_id={plant.type_id}
+                plant_id={plant.plant_id}
+                type={plant.type}
+                plant_name={plant.plant_name}
+                plant_description={plant.plant_description}
+              />;
+            })}
+          </ul>
+        </section>
+      );
+    }
+    // filters applied
+    else {
+      return (
+        <section className="drawer">
+          <SearchFilters
+            list={list}
+            filteredList={filteredList}
+            setFilteredList={setFilteredList}
+          />
+          <ul className="plant-list">
+            {filteredList.map((plant) => {
+              <Draggable
+                key={plant.plant_id}
+                type_id={plant.type_id}
+                plant_id={plant.plant_id}
+                type={plant.type}
+                plant_name={plant.plant_name}
+                plant_description={plant.plant_description}
+              />;
+            })}
+          </ul>
+        </section>
+      );
+    }
+  }
+  // Planter Drawer
+  else {
+    const { dndPlantList, setDndPlantList } = props;
+    // no filters applied
+    if (!filteredList) {
+      return (
+        <section className="drawer">
+          <SearchFilters
+            list={list}
+            filteredList={filteredList}
+            setFilteredList={setFilteredList}
+          />
+          <ul className="plant-list">
+            {list.map((plant) => {
+              <Draggable
+                dndPlantList={dndPlantList}
+                setDndPlantList={setDndPlantList}
+                key={plant.plant_id}
+                type_id={plant.type_id}
+                plant_id={plant.plant_id}
+                type={plant.type}
+                plant_name={plant.plant_name}
+                plant_description={plant.plant_description}
+              />;
+            })}
+          </ul>
+        </section>
+      );
+    }
+    // filters applied
+    else {
+      return (
+        <section className="drawer">
+          <SearchFilters
+            list={list}
+            filteredList={filteredList}
+            setFilteredList={setFilteredList}
+          />
+          <ul className="plant-list">
+            {filteredList.map((plant) => {
+              <Draggable
+                dndPlantList={dndPlantList}
+                setDndPlantList={setDndPlantList}
+                key={plant.plant_id}
+                type_id={plant.type_id}
+                plant_id={plant.plant_id}
+                type={plant.type}
+                plant_name={plant.plant_name}
+                plant_description={plant.plant_description}
+              />;
+            })}
+          </ul>
+        </section>
+      );
+    }
+  }
+}
