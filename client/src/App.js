ee3e80ecce604f93c763b6f60d606d332bdd29c6
GravityPope <hijenkins@live.ca>
2024-05-05 13:02:15 -0600

    add footer

--- client/src/App.js
+++ client/src/App.js
@@ -1,20 +1,21 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import PlannerPage from "./pages/PlannerPage/PlannerPage";
 import Header from "./components/Header/Header";
+import Footer from "./components/Footer/Footer";
 
 //TODO: make Header, delete functionality for planters
-
 
 function App() {
   return (
     <div className="body">
-    <BrowserRouter>
-    <Header/>
-    <Routes>
-      <Route path="/" element={<PlannerPage/>} />
-    </Routes>
-    </BrowserRouter>
-  </div>
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path="/" element={<PlannerPage />} />
+        </Routes>
+        <Footer />
+      </BrowserRouter>
+    </div>
   );
 }
 
---
+++ client/src/components/Footer/Footer.jsx
@@ -0,0 +1,18 @@
+import "./Footer.scss";
+
+export default function Footer() {
+  return (
+    <section className="footer">
+      <p className="footer__copy">
+        Made With ❤️ by{" "}
+        <a
+          target="_blank"
+          href="https://www.linkedin.com/in/hi-hannah-jenkins/"
+        >
+          Hannah Jenkins
+        </a>{" "}
+        2024 &copy;
+      </p>
+    </section>
+  );
+}
